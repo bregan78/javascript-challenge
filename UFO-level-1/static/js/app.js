ca21095f4cbd937c4204a804745326155252be92
Brian Regan <bregan7879@gmail.com>
2020-12-14 14:56:14 -0500

    finished part 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,10 +8,15 @@ var form1 = d3.select("#form");
 
 button1.on("click", runEnter);
 form1.on("submit",runEnter);
-// YOUR CODE HERE!
+
+var list = d3.select("#tablebody");
+list.html("");
+
+
+
 function runEnter() {
 
-    //d3.event.preventDefault();
+    d3.event.preventDefault();
 
     var inputElement = d3.select("#datetime");
 
@@ -23,33 +28,35 @@ function runEnter() {
     var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
     var list = d3.select("#tablebody");
     list.html("");
+    setuptable(filteredData);
 
-
-    filteredData.forEach(function(element) {
+        /*filteredData.forEach(function(element) {
            console.log(element);
-           var row = tbody.append("tr");
+           var row = list.append("tr");
            Object.entries(element).forEach(function([key, value]) {
                 console.log(key, value);
 
                 //Append a cell to the row for each value
-                
-
-
-                //var cell = row.append("td");
-                //cell.text(value);
-
-
-
-
-         });
-        
-
-    console.log(filteredData);
-
-    var list = d3.select("#tablebody");
-
+                var cell = row.append("td");
+                cell.text(value);
+            });
+        }); */
     
-
-    list.append("tr").append("td").text(`${date}`);
-
+      
 };
+function setuptable(somedata) {
+
+    somedata.forEach(function(element) {
+        console.log(element);
+        var row = list.append("tr");
+        Object.entries(element).forEach(function([key, value]) {
+            console.log(key, value);
+
+            //Append a cell to the row for each value
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+setuptable(tableData);
